509b6ca0cb01bcfe4bd8f09f2e4445c5ab9a70c0
LeJeu <64744459+le-jeu@users.noreply.github.com>
2020-08-29 12:57:33 +0200

    add black skin

---
+++ dist/Black-wasabee-skin.meta.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @id 	 Black-Wasabee-Skin
+// @name 	 Black
+// @namespace 	 https://wasabee.rocks/
+// @version 	 0.0.2
+// @updateURL 	 https://le-jeu.github.io//dist/Black-wasabee-skin.meta.js
+// @downloadURL 	 https://le-jeu.github.io//dist/Black-wasabee-skin.user.js
+// @description 	 Black skin for Wasabee
+// @author 	 Wasabee Project Team
+// @include 	 /https?:\/\/.*\.ingress\.com\/?((intel|mission)?(\/?(\?|#).*)?)?/
+// @category 	 Other
+// @grant 	 none
+// ==/UserScript==
+
