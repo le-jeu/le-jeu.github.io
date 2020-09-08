e75dfd02cdb21f2b8549300aafffd7219f468dea
LeJeu <64744459+le-jeu@users.noreply.github.com>
2020-09-08 19:49:24 +0200

    add drawtools anchors

---
+++ dist/draw_tools_anchors-wasabee-skin.meta.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @id 	 draw_tools_anchors-Wasabee-Skin
+// @name 	 Draw tools icons for anchors
+// @namespace 	 https://wasabee.rocks/
+// @version 	 0.0.1
+// @updateURL 	 https://le-jeu.github.io/dist/draw_tools_anchors-wasabee-skin.meta.js
+// @downloadURL 	 https://le-jeu.github.io/dist/draw_tools_anchors-wasabee-skin.user.js
+// @description 	 Wasabee: Draw tools icons for anchors
+// @author 	 LeJeu
+// @include 	 /https?:\/\/.*\.ingress\.com\/?((intel|mission)?(\/?(\?|#).*)?)?/
+// @category 	 Other
+// @grant 	 none
+// ==/UserScript==
+
