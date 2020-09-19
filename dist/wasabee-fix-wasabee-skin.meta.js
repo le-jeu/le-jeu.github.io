4f7e25bfe2e68d91451da654935c1fc1525a85ca
LeJeu <64744459+le-jeu@users.noreply.github.com>
2020-09-19 19:02:55 +0200

    update wasabee-fix

--- dist/wasabee-fix-wasabee-skin.meta.js
+++ dist/wasabee-fix-wasabee-skin.meta.js
@@ -2,11 +2,11 @@
 // @id 	 wasabee-fix-Wasabee-Skin
 // @name 	 wasabee-fix
 // @namespace 	 https://wasabee.rocks/
-// @version 	 0.0.2
+// @version 	 0.0.3
 // @updateURL 	 https://le-jeu.github.io/dist/wasabee-fix-wasabee-skin.meta.js
 // @downloadURL 	 https://le-jeu.github.io/dist/wasabee-fix-wasabee-skin.user.js
 // @description 	 some UI tweaks for Wasabee
-// @author 	 Wasabee Project Team
+// @author 	 LeJeu
 // @include 	 /https?:\/\/.*\.ingress\.com\/?((intel|mission)?(\/?(\?|#).*)?)?/
 // @category 	 Other
 // @grant 	 none
