d914ce6f8d84914bd6174394fdf5e532c2c9417d
LeJeu <64744459+le-jeu@users.noreply.github.com>
2020-10-06 00:07:33 +0200

    add labels_layer

---
+++ dist/labels_layer.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @author         jaiperdu
+// @name           IITC plugin: Default base maps with labels above fields
+// @category       Map Tiles
+// @version        0.1.0
+// @description    Print labels as an overlay of intel layer
+// @id             labels_layer
+// @updateURL    https://le-jeu.github.io/dist/labels_layer.meta.js
+// @downloadURL    https://le-jeu.github.io/dist/labels_layer.user.js
+// @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
+// @match          https://intel.ingress.com/*
+// @grant          none
+// ==/UserScript==
