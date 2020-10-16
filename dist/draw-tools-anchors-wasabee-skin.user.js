c82d0a73759c7c2bb02b837b078100280f00c24a
LeJeu <64744459+le-jeu@users.noreply.github.com>
2020-10-16 12:41:01 +0200

    update thegame-svg

--- dist/draw-tools-anchors-wasabee-skin.user.js
+++ dist/draw-tools-anchors-wasabee-skin.user.js
@@ -1,15 +1,15 @@
 // ==UserScript==
-// @id   draw-tools-anchors-wasabee-skin
-// @name   Wasabee Skin: Draw Tools Anchors
-// @namespace    https://wasabee.rocks/
-// @version    0.0.3
-// @updateURL    https://le-jeu.github.io/dist/draw-tools-anchors-wasabee-skin.meta.js
-// @downloadURL    https://le-jeu.github.io/dist/draw-tools-anchors-wasabee-skin.user.js
-// @description    Draw tools icons for anchors
-// @author   LeJeu
-// @include    /https?:\/\/.*\.ingress\.com\/?((intel|mission)?(\/?(\?|#).*)?)?/
-// @category   Appearance
-// @grant    none
+// @id 	 draw-tools-anchors-wasabee-skin
+// @name 	 Wasabee Skin: Draw Tools Anchors
+// @namespace 	 https://wasabee.rocks/
+// @version 	 0.0.3
+// @updateURL 	 https://le-jeu.github.io/dist/draw-tools-anchors-wasabee-skin.meta.js
+// @downloadURL 	 https://le-jeu.github.io/dist/draw-tools-anchors-wasabee-skin.user.js
+// @description 	 Draw tools icons for anchors
+// @author 	 LeJeu
+// @include 	 /https?:\/\/.*\.ingress\.com\/?((intel|mission)?(\/?(\?|#).*)?)?/
+// @category 	 Appearance
+// @grant 	 none
 // ==/UserScript==
 
 
