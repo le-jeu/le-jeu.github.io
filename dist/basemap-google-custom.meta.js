97e739adf74a33c134a33863e9e1f60bb1fd1300
LeJeu <64744459+le-jeu@users.noreply.github.com>
2021-04-30 12:12:10 +0200

    symlinks

--- dist/basemap-google-custom.meta.js
+++ dist/basemap-google-custom.meta.js
@@ -1,13 +1,1 @@
-// ==UserScript==
-// @author         jaiperdu
-// @name           IITC plugin: Custom Google map
-// @category       Map Tiles
-// @version        0.1.1
-// @description    Add a customizable Version of Google map tiles as a base layer.
-// @id             basemap-google-custom
-// @updateURL      https://le-jeu.github.io/dist/basemap-google-custom.meta.js
-// @downloadURL    https://le-jeu.github.io/dist/basemap-google-custom.user.js
-// @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
-// @match          https://intel.ingress.com/*
-// @grant          none
-// ==/UserScript==
+basemap-google-custom.user.js
--- dist/portals-pictures.meta.js
+++ dist/portals-pictures.meta.js
@@ -1,13 +1,1 @@
-// ==UserScript==
-// @author         jaiperdu
-// @name           IITC plugin: Portals pictures
-// @category       Info
-// @version        0.1.1
-// @description    Show portal pictures in a dialog
-// @id             portals-pictures
-// @updateURL      https://le-jeu.github.io/dist/portals-pictures.meta.js
-// @downloadURL    https://le-jeu.github.io/dist/portals-pictures.user.js
-// @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
-// @match          https://intel.ingress.com/*
-// @grant          none
-// ==/UserScript==
+portals-pictures.user.js
